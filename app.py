58caaa6a37cbd611b6128ddeeeb88d79f952fe0b
krishbaliga <121359034+krishbaliga@users.noreply.github.com>
2025-05-30 11:37:06 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from PIL import Image
+
+uploaded_files = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+if st.button("Show Image"):
+    if uploaded_files:
+        for file in uploaded_files:
+            def process_images(img):
+                return img.size
+            res = process_images(Image.open(file))
+            st.write(f"{file.name} - {res}")
+    else:
+        st.warning("No Files Uploaded")
