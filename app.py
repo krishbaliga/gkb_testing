28003294641d028f964d6275c1144e9a1ab19d64
krishbaliga <121359034+krishbaliga@users.noreply.github.com>
2025-06-02 09:54:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if st.button("Show Image"):
             url = f"https://picsum.photos/{width}/{height}"
             if grayscale:
                 url += "?grayscale"
-
+                
             response = requests.get(url)
             if response.status_code == 200:
                 edited_image = Image.open(BytesIO(response.content))
